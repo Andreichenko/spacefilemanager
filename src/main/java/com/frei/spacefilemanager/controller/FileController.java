67fcc46d6b69eb6f03c586d380c10985c2b37de8
aleksandrandreichenko <andreichenko@web.de>
2023-11-23 01:14:57 +0200

    Created Spring project for RestAPI + AWS config + File Controller + Service
    added
    list of data from the bucket

--- src/main/java/com/frei/spacefilemanager/controller/FileController.java
+++ src/main/java/com/frei/spacefilemanager/controller/FileController.java
@@ -5,17 +5,31 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import com.frei.spacefilemanager.service.FileService;
-
+import org.springframework.core.io.InputStreamResource;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import java.io.InputStream;
 import java.util.List;
 
 @RestController
 @RequestMapping("/files")
 public class FileController {
+
     @Autowired
     private FileService fileService;
 
     @GetMapping
     public List<String> listFiles() {
         return fileService.listFiles();
+    }
+
+    @GetMapping("/{fileName}")
+    public ResponseEntity<InputStreamResource> downloadFile(@PathVariable String fileName) {
+        InputStream inputStream = fileService.downloadFile(fileName);
+        InputStreamResource resource = new InputStreamResource(inputStream);
+
+        return ResponseEntity.ok()
+                .header("Content-Disposition", "attachment; filename=" + fileName)
+                .body(resource);
     }
 }
--- src/main/java/com/frei/spacefilemanager/service/FileService.java
+++ src/main/java/com/frei/spacefilemanager/service/FileService.java
@@ -4,22 +4,31 @@ import org.springframework.stereotype.Service;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.model.ListObjectsV2Result;
 import com.amazonaws.services.s3.model.S3ObjectSummary;
+import com.amazonaws.services.s3.model.S3Object;
+import com.amazonaws.services.s3.model.GetObjectRequest;
 import java.util.List;
 import java.util.stream.Collectors;
+import java.io.InputStream;
 
 @Service
 public class FileService {
 
     private final AmazonS3 s3Client;
+    private final String bucketName = "callbot";
 
     public FileService(AmazonS3 s3Client) {
         this.s3Client = s3Client;
     }
 
     public List<String> listFiles() {
-        ListObjectsV2Result result = s3Client.listObjectsV2("callbot");
+        ListObjectsV2Result result = s3Client.listObjectsV2(bucketName);
         return result.getObjectSummaries().stream()
                 .map(S3ObjectSummary::getKey)
                 .collect(Collectors.toList());
+    }
+
+    public InputStream downloadFile(String fileName) {
+        S3Object s3object = s3Client.getObject(new GetObjectRequest(bucketName, fileName));
+        return s3object.getObjectContent();
     }
 }
