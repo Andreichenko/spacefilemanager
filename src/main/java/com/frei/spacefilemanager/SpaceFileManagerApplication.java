260268bcd783bb95307a805a37da9b6a988f6831
aleksandrandreichenko <andreichenko@web.de>
2023-11-22 22:46:29 +0200

    Initial commit

---
+++ src/main/java/com/frei/spacefilemanager/SpaceFileManagerApplication.java
@@ -0,0 +1,13 @@
+package com.frei.spacefilemanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpaceFileManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpaceFileManagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/frei/spacefilemanager/SpaceFileManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.frei.spacefilemanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpaceFileManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
