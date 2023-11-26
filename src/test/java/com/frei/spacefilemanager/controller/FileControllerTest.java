fc72c1f46f914f0df035c186c8c8b4ac9decb14d
aleksandrandreichenko <andreichenko@web.de>
2023-11-26 16:30:47 +0200

    CAL-604/644 Functionality for viewing the list of files in the Spaces bucket
    added test
    added security config and authorization

--- src/test/java/com/frei/spacefilemanager/controller/FileControllerTest.java
+++ src/test/java/com/frei/spacefilemanager/controller/FileControllerTest.java
@@ -9,8 +9,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
+
 import java.util.Arrays;
 import java.util.Date;
+
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
